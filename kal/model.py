9fefe822ce8fc43cd4fd429ed96a0f91f2db3138
mozzielol <mozzielol@hotmail.com>
2018-10-11 10:47:00 +0100

    Update model.py

--- kal/model.py
+++ kal/model.py
@@ -8,21 +8,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def mse(cur_grad,pre_grad):
-	res = []
-	for i in range(len(pre_grad)):
-		res.append(K.sum(K.square(cur_grad[i] - pre_grad[i])))
-	return sum(res)/len(res)
-
-
-def Custom_loss(model,pre_grad,batch,X,y):
-	def loss(y_true,y_pred):
-		return K.categorical_crossentropy(y_true, y_pred) + mse(get_gradients(model,X[batch*128 : (batch+1)*128],y[batch*128 : (batch+1)*128]),pre_grad)
-	return loss
-
 
 class cnn_model(object):
-
+	'''
+	 - Initialize the model
+  	 - important parameters:
+  	 	- history: it will record all the accuracy on training dataset and validation dataset
+	'''
 	def __init__(self):
 		self.dim = 64
 		self.num_classes = 10
@@ -55,14 +47,21 @@ class cnn_model(object):
 		embeddings_layer_names=None, 
 		embeddings_metadata=None)
 
+
+	#What data is used for validation
+	def val_data(self,X_test,y_test):
+		self.X_test = X_test
+		self.y_test = y_test
+
+	#train the model by normal gradient descent algorithm
 	def fit(self,X_train,y_train):
 		self.pre_x = X_train
 		self.pre_y = y_train
 		self.model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
 		if self.history is None:
-			self.history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[self.tbCallBack],validation_data=(X_train,y_train))
+			self.history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,validation_data=(self.X_test,self.y_test))
 		else:
-			history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[self.tbCallBack],validation_data=(X_train,y_train))
+			history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,validation_data=(self.X_test,self.y_test))
 			self.history.history['acc'].extend(history.history['acc'])
 			self.history.history['val_acc'].extend(history.history['val_acc'])
 
@@ -72,27 +71,43 @@ class cnn_model(object):
 		X = X_train
 		y = y_train
 
+	'''
+	 - This function is used for 'kal' algorithm.
+	 - The model will calculate the gradient on D1[batch0], and never access to D1 again
+	'''
 	def transfer(self,X_train,y_train):
 		opcallback = op_pre_callback(X_train,y_train)
 		self.model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
-		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback,self.tbCallBack],validation_data=(X,y))
+		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback],validation_data=(self.X_test,self.y_test))
 		self.history.history['acc'].extend(history.history['acc'])
 		self.history.history['val_acc'].extend(history.history['val_acc'])
 
+	'''
+	 - This function is used for 'kal_pre' algorithm
+	 - The model will access to D1 to calculate the gradient during all training process
+	'''
 	def use_pre(self,X_train,y_train):
 		opcallback = op_pre_callback(X,y,True)
 		self.model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
-		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback,self.tbCallBack],validation_data=(X,y))
+		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback],validation_data=(self.X_test,self.y_test))
 		self.history.history['acc'].extend(history.history['acc'])
 		self.history.history['val_acc'].extend(history.history['val_acc'])
 
+	'''
+	 - This funciton is used for 'kal_cur' algorithm
+	 - The model won't access to D1. All the gradients are calculated on current training data
+	'''
 	def use_cur(self,X_train,y_train):
 		opcallback = op_pre_callback(X_train,y_train,True)
 		self.model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
-		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback,self.tbCallBack],validation_data=(X,y))
+		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[opcallback],validation_data=(self.X_test,self.y_test))
 		self.history.history['acc'].extend(history.history['acc'])
 		self.history.history['val_acc'].extend(history.history['val_acc'])
 
+	'''
+	 - A simple transer learning function
+	 - It will pop the last two layers, and add another classifier to the end of the model
+	'''
 	def nor_trans(self,X_train,y_train):
 		self.model.pop()
 		self.model.pop()
@@ -102,7 +117,7 @@ class cnn_model(object):
 		self.model.add(Dense(32,activation='relu'))
 		self.model.add(Dense(self.num_classes,activation='softmax'))
 		self.model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
-		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,callbacks=[self.tbCallBack],validation_data=(X_train,y_train))
+		history = self.model.fit(X_train,y_train,epochs=self.epoch,batch_size=128,verbose=True,validation_data=(self.X_test,self.y_test))
 		self.history.history['acc'].extend(history.history['acc'])
 		self.history.history['val_acc'].extend(history.history['val_acc'])
 
@@ -121,6 +136,9 @@ class cnn_model(object):
 	def get_history(self):
 		return self.history
 
+	'''
+	Plot the history of accuracy
+	'''
 	def plot(self,name,model,shift=2):
 		plt.subplot(211)
 		plt.title('accuracy on current training data')
@@ -146,29 +164,17 @@ class cnn_model(object):
 
 
 
-class grad_knowledge(Callback):
-	"""docstring for grad_knowledge"""
-	def __init__(self,pre_x,pre_y,X_train,pre_grad,batch):
-		super(grad_knowledge, self).__init__()
-		self.pre_grad = pre_grad
-		self.pre_x = pre_x
-		self.pre_y = pre_y
-		self.X_train = X_train
-		self.batch = batch
-	
-	def on_epoch_begin(self,epoch,logs={}):
-		self.epoch = epoch
 
-	
-	def on_batch_begin(self,batch,logs={}):
-		#index = most_similar(self.pre_x,self.X_train[batch*128 : (batch+1)*128])
-		grad = get_weight_grad(self.model,self.pre_x[batch*128 : (batch+1)*128],self.pre_y[batch*128 : (batch+1)*128])
-		K.set_value(self.pre_grad,np.array(grad))
-		K.set_value(self.batch , batch)
-		
+'''
+Kalman Callback.
+	- use_pre: if is true, the model will calculate the gradients on X_tarin,y_train
+		|- in 'kal_pre' algorithm, the X_train,y_train will be D1
+		|- in 'kal_cur' altorithm, the X_train,y_train will be current training data
+	- X_train, y_train will be
+		|- 'kal', 'kal_cur' : The training data (D2,or D3)
+		|- 'kal_pre': Always D1
 
-#
-
+'''
 class op_pre_callback(Callback):
 	"""docstring for op_batch_callback"""
 	def __init__(self,X_train,y_train,use_pre=False):
@@ -179,7 +185,7 @@ class op_pre_callback(Callback):
 
 
 
-
+	#Calculate the Kalman Gain based on current gradients and previous gradients
 	def Kal_gain(self,cur_grad,pre_grad):
 		res = []
 		for i in range(len(pre_grad)):
@@ -188,21 +194,30 @@ class op_pre_callback(Callback):
 			res.append(temp)
 		return res
 
-
+	#Calculate the gradients of model on D1[ batch_0 ]
+	#It will be used in 'kal'. 
+	#	|- For other algorithm, the self.pre_g will be updated. 
+	#	|- For 'kal', self.pre_g will not be updated
 	def on_train_begin(self,logs={}):
 		self.pre_g = get_weight_grad(self.model,X[0*128:(0+1)*128],y[0*128:(0+1)*128])
 
 	def on_epoch_begin(self,epoch,logs={}):
 		self.epoch = epoch
 		
-
+	#At the begining of each batch, get the weights
+	#if use previous knowledge, update previous gradients(self.pre_g)
 	def on_batch_begin(self,batch,logs={}):
 		self.pre_w = get_weights(self.model)
 		if self.use_pre:
 			self.pre_g = get_weight_grad(self.model,self.pre_x[batch*128:(batch+1)*128],self.pre_y[batch*128:(batch+1)*128])
 		
 
-
+	#At the end of the batch:
+	# |- Get the current weights
+	# |- Calculate the gradients of model on X_train,y_train
+	# |- Calculate the Kalman Gain
+	# |- Kalman Filter to calculate the new weights and set_weights
+	# |- Update error(self.pre_g): This will be used in 'kal' only. Other algorithm will update at the batch begin
 	def on_batch_end(self,batch,logs={}):
 		
 		
@@ -228,57 +243,6 @@ class op_pre_callback(Callback):
 		
 		
 
-	
-class grad_check(Callback):
-	"""docstring for grad_check"""
-	def __init__(self, X_train,y_train):
-		super(grad_check, self).__init__()
-		self.X_train = X_train
-		self.y_train = y_train
-
-	def on_batch_end(self,batch,logs={}):
-		cur_g = get_weight_grad(self.model,self.X_train[batch*128:(batch+1)*128],self.y_train[batch*128:(batch+1)*128])
-		print('cur g :', self.cur_g[0][0][0][0][0])
-
-
-class test_callback(Callback):
-	"""docstring for test_callback"""
-	def __init__(self, X_train,y_train,pre_x, pre_y):
-		super(test_callback, self).__init__()
-		self.X_train = X_train
-		self.y_train = y_train
-		self.pre_x = pre_x
-		self.pre_y = pre_y
-
-	def on_batch_begin(self,batch,logs={}):
-		self.pre_w = get_weights(self.model)
-		self.pre_g = get_weight_grad(self.model,self.pre_x[batch*128:(batch+1)*128],self.pre_y[batch*128:(batch+1)*128])
-		self.cur_g = get_weight_grad(self.model,self.X_train[batch*128:(batch+1)*128],self.y_train[batch*128:(batch+1)*128])
-
-	def on_batch_end(self,batch,logs={}):
-		
-		
-		self.cur_w = get_weights(self.model)
-
-		self.cur_g = get_weight_grad(self.model,self.pre_x[batch*128:(batch+1)*128],self.pre_y[batch*128:(batch+1)*128])
-
-		
-		Kalman_gain = self.Kal_gain(self.cur_g,self.pre_g)
-		new_w = []
-
-		for P,Z,E in zip(self.pre_w,self.cur_w,Kalman_gain):
-			new_w.append(P + (Z-P) * E )
-
-		
-		self.model.set_weights(new_w)
-		
-		new_g = []
-		for kal,g in zip(Kalman_gain,self.pre_g):
-			new_g.append((1- kal) * g )
-
-		self.pre_g = new_g
-
-		
 
 
 
